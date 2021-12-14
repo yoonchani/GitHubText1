7c08f78fcc82eef19219f4af950fa674be876685
yoonchani <dbscks0801@naver.com>
2021-12-14 18:40:02 +0900

    IntelliJ GitHub Test

---
+++ src/main/java/com/idea/githubtext1/GitHubText1Application.java
@@ -0,0 +1,13 @@
+package com.idea.githubtext1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubText1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubText1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/idea/githubtext1/GitHubText1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.githubtext1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubText1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
